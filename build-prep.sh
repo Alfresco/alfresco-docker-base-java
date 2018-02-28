b4d3301990cb2e073b0524deeb37249100828ffa
Nic Doye <nic.doye@alfresco.com>
2018-02-28 17:35:11 +0000

    DEPLOY-386 Use mvn to get Java

--- build-prep.sh
+++ build-prep.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
+
 set -o errexit
 
 . ./build.properties
 
-JRE_FILENAME=`echo "${JRE_URL##*/}"`
+# If you want the current release
 
-curl -jksSLOH "Cookie: oraclelicense=accept-securebackup-cookie" $JRE_URL
+if [ "${USE_MVN}" = 'true' ]; then
+
+    # As Oracle have made downloading non-current versions of Java difficult,
+    # we are sadly having to store them in an internal repository.
+
+    mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.2:copy \
+        -DrepoUrl=https://artifacts.alfresco.com/nexus/content/repositories/oracle-java \
+        -Dartifact="${JAVA_OS_ARCH}":"${JAVA_SE_TYPE}":"${JAVA_VERSION}":"${JAVA_PACKAGING}":bin \
+        -DoutputDirectory=.
+
+    JRE_FILENAME="${JAVA_SE_TYPE}"-"${JAVA_VERSION}"-bin."${JAVA_PACKAGING}"
+else
+    JRE_FILENAME=`echo "${JRE_URL##*/}"`
+
+    curl -jksSLOH "Cookie: oraclelicense=accept-securebackup-cookie" $JRE_URL
+fi
 
 if [ -x "$(command -v shasum)" ]; then
   echo "$JRE_CHECKSUM_256  $JRE_FILENAME" | shasum -a 256 -c -
