e732645e5f563b2e5864339ef9b4c8109317e3ed
Nic Doye <nic.doye@alfresco.com>
2018-03-01 13:45:57 +0000

    DEPLOY-386 Make DOCKER_IMAGE_TAG source of truth for Java version. Make Checksum work for all versions of serverjre on linux-x64

--- build-prep.sh
+++ build-prep.sh
@@ -4,13 +4,15 @@ set -o errexit
 
 . ./build.properties
 
-# If you want the current release
+# Get Java release from Docker tag
+JAVA_VERSION="${DOCKER_IMAGE_TAG%%-*}"
 
 if [ "${USE_MVN}" = 'true' ]; then
 
     # As Oracle have made downloading non-current versions of Java difficult,
     # we are sadly having to store them in an internal repository.
 
+    # Use Maven 3
     unset M2_HOME
     export M3_HOME=/opt/apache-maven
 
@@ -19,15 +21,23 @@ if [ "${USE_MVN}" = 'true' ]; then
         -Dartifact="${JAVA_OS_ARCH}":"${JAVA_SE_TYPE}":"${JAVA_VERSION}":"${JAVA_PACKAGING}":bin \
         -DoutputDirectory=.
 
+    # Our filenames are munged into Maven compatible names
     JRE_FILENAME="${JAVA_SE_TYPE}"-"${JAVA_VERSION}"-bin."${JAVA_PACKAGING}"
 else
-    JRE_FILENAME=`echo "${JRE_URL##*/}"`
 
-    curl -jksSLOH "Cookie: oraclelicense=accept-securebackup-cookie" $JRE_URL
+    # You can still download the latest Java version from Oracle
+
+    curl -jksSLOH "Cookie: oraclelicense=accept-securebackup-cookie" "${JRE_URL}"
+
+    JRE_FILENAME="${JRE_URL##*/}"
 fi
 
+# bash 3.2 compatible alternative to associative arrays
+JAVA_VERSION_NO_DOTS=$(echo $JAVA_VERSION | sed -e 's/\.//g')
+JRE_CHECKSUM_256_REF="JRE_CHECKSUM_256_${JAVA_VERSION_NO_DOTS}"
+
 if [ -x "$(command -v shasum)" ]; then
-  echo "$JRE_CHECKSUM_256  $JRE_FILENAME" | shasum -a 256 -c -
+  echo "${!JRE_CHECKSUM_256_REF} ${JRE_FILENAME}" | shasum -a 256 -c -
 else
-  echo "$JRE_CHECKSUM_256  $JRE_FILENAME" | sha256sum -c -
+  echo "${!JRE_CHECKSUM_256_REF} ${JRE_FILENAME}" | sha256sum -c -
 fi
