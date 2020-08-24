c5430a9fdf39530c0f33d7add76110d4cda59564
Sergiu Vidrascu <vsergiu@hotmail.com>
2020-08-24 19:26:34 +0300

    Update travis build tagging and make parallel

--- scripts/etc/common.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Alfresco Repo
-# Stored like "linux-x64:serverjre:8u181:tar.gz:bin"
-export artifacts_repo_url='https://artifacts.alfresco.com/nexus/content/repositories/oracle-java'
-
-# Download directory. Originally meant to be a subdirectory,
-# for when we were going to have separate Dockerfiles.
-export docker_build_dir='.'
